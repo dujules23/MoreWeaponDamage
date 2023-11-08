29d8e36f07120d82ff07f287b2730f534d5522f9
Durrell Jules <dujules23@gmail.com>
2023-11-07 23:44:47 -0500

    created new Main method and ArrowDamage class, added SwordDamage class from previous project

---
+++ MoreWeaponDamage/ArrowDamage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace MoreWeaponDamage
+{
+	public class ArrowDamage
+	{
+		public ArrowDamage(int startingRoll)
+		{
+            roll = startingRoll;
+            CalculateDamage();
+            Console.WriteLine("Created an instance of ArrowDamage");
+        }
+
+        private const decimal BASE_MULTIPLIER = 0.35M;
+        private const decimal MAGIC_MULTIPLIER = 2.5M;
+        private const decimal FLAME_DAMAGE = 1.23M;
+
+        public int Damage { get; private set; }
+
+        // Removed old fields and created backing field and property for roll, flaming, and magic.
+        private int roll;
+
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool flaming;
+
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool magic;
+
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+
+
+        private void CalculateDamage()
+        {
+            decimal baseDamage = Roll * BASE_MULTIPLIER;
+            if (Magic) baseDamage *= MAGIC_MULTIPLIER;
+            if (Flaming) Damage = (int)Math.Ceiling(baseDamage + FLAME_DAMAGE);
+            else Damage = (int)Math.Ceiling(baseDamage);
+        }
+    }
+}
+
---
+++ MoreWeaponDamage/SwordDamage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+namespace MoreWeaponDamage
+{
+	public class SwordDamage
+	{
+		public SwordDamage(int startingRoll)
+		{
+			roll = startingRoll;
+			CalculateDamage();
+			Console.WriteLine("Created an instance of SwordDamage");
+		}
+
+		private const int BASE_DAMAGE = 3;
+		private const int FLAME_DAMAGE = 2;
+
+        public int Damage { get; private set; }
+
+        // Removed old fields and created backing field and property for roll, flaming, and magic.
+        private int roll;
+
+		public int Roll
+		{
+			get { return roll; }
+			set
+			{
+				roll = value;
+				CalculateDamage();
+			}
+		}
+
+		private bool flaming;
+
+		public bool Flaming
+		{
+			get { return flaming; }
+			set
+			{
+				flaming = value;
+				CalculateDamage();
+			}
+		}
+
+		private bool magic;
+
+		public bool Magic
+		{
+			get { return magic; }
+			set
+			{
+				magic = value;
+				CalculateDamage();
+			}
+		}
+
+		
+
+		private void CalculateDamage()
+		{
+			decimal magicMultiplier = 1M;
+			if (Magic) magicMultiplier = 1.75M;
+
+			Damage = BASE_DAMAGE;
+			Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+			if (Flaming) Damage += FLAME_DAMAGE;
+			Console.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+		}
+
+	}
+}
+
